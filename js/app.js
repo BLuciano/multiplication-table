9361fc22ada24e38f187e3942ab464d48016a3cb
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-28 10:07:56 -0700

    Added cntrl functions to change numbers dynamically

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 angular.module('myApp', [])
-  .controller('MultiplicationCtrl', function($scope){
-    $scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+  .controller('MultiplicationCtrl', function($scope, $attrs){
+    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+    $scope.numbers = function(){
+      var numbers = [];
+      for(var i = 0; i < $scope.numberLimit; i++){
+        numbers[i] = i + 1;
+      }
+      return numbers;
+    };
+
+    $scope.compute = function(a, b){
+      return a * b;
+    };
   });
